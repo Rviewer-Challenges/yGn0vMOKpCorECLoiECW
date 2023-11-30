649da09dd9b96772e5db95a91d9970363bc76a4c
mapten <marcosprietosi@gmail.com>
2023-11-30 23:14:10 +0100

    feat: difficulties configuration

---
+++ src/configuration/game.configuration.ts
@@ -0,0 +1,17 @@
+export const emojis = ['🎅', '🎄', '❄️', '🎁', '🥂', '🌠', '☃️', '⛄',
+  '🫎', '🌨️', '🏂', '🤶', '🧑‍🎄', '🎉', '💫', '🧦', '🥛', '🧝', '🧝‍♂️',
+  '🧝‍♀️', '🌟', '🍪', '🦌', '🕯️', '👼', '🥳', '🎊'];
+
+export const dificulties = [{
+  level: 'easy',
+  verticalCards: 4,
+  horizontalCards: 4,
+}, {
+  level: 'medium',
+  verticalCards: 4,
+  horizontalCards: 6,
+}, {
+  level: 'hard',
+  verticalCards: 5,
+  horizontalCards: 6,
+}];
